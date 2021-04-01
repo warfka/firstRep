0918892bde3a3f1ab530822728446713636ac051
MikitaZvezd <loanclif@gmail.com>
2021-04-01 15:09:20 +1000

    3commit LAB4

--- lb1.cpp
+++ lb1.cpp
@@ -5,5 +5,27 @@ using namespace std;
 
 int main()
 {
+  char str[1000];
+    int i, j;
+
+    cin >> str;
+    int len = strlen(str);
+    for (i = 0; i < len; i++) {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            do {
+                for (j = i; j < len; j++) {
+                    str[j] = str[j + 1];
+                }
+            } while (str[i] >= '0' && str[i] <= '9');
+            
+            len--;
+        }
+
+    }
+   str[len + 1] = '\0';
+    cout << str << endl;
+    return 0;
+}
 
 
